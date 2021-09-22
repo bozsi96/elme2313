10194a88209401568203de7ab6769d6658b3fb3c
Frey Balázs <frey.balazs96@gmail.com>
2021-09-22 18:22:01 +0200

    New figures

--- HW1/figure_labels.m
+++ HW1/figure_labels.m
@@ -1,7 +1,7 @@
-title("Simplified model")
-legend('off')
-ylabel("Speed (rad/s)")
-xlabel('Time (s)')
+title("Current comparison")
+ylabel("Current (A)")
+xlabel("Time (s)")
 grid minor
 xline(0.2, '--r')
 xline(0.3, '--r')
+legend('Complete model', 'Simple model')
